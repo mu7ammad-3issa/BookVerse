1c4119dd1691d90f1df9601087aafd2a0f3f69d1
3issa <mu7ammad.3issa@gmail.com>
2024-11-11 22:26:36 +0200

    create Server failure from dio exception

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,57 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate error');
+      case DioExceptionType.badResponse:
+        ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Unexpected ConnectionError, Please try later!');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+
+      default:
+        return ServerFailure('Unexpected Error, Please try again!');
+    }
+    return ServerFailure('Oops There was an error, Please try again');
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    switch (statusCode) {
+      case 400:
+      case 401:
+      case 403:
+        return ServerFailure(response['error']['message']);
+
+      case 404:
+        return ServerFailure('Your request not found, Please try later!');
+
+      case 500:
+        return ServerFailure('Internal Server error, Please try later!');
+
+      default:
+        return ServerFailure('Oops There was an error, Please try later!');
+    }
+  }
+}
